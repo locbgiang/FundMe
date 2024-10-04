e13a09f9ce4114343123e72f9c2052f9bb5e1654
locgiang <locbgiang@gmail.com>
2024-10-03 23:02:20 -0700

    finished interactions

--- backend/script/Interactions.s.sol
+++ backend/script/Interactions.s.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
-import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
     uint256 constant SEND_VALUE = 0.01 ether;
 
     function fundFundMe(address mostRecentlyDeployed) public {
-        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE};
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
@@ -18,8 +20,22 @@ contract FundFundMe is Script {
             "FundMe",
             block.chainid
         );
-        vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
         vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        withdrawFundMe(mostRecentlyDeployed);
     }
 }
