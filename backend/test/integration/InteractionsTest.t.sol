2858a468fed29206309c8ebc652b771ed79bb80c
locgiang <locbgiang@gmail.com>
2024-10-04 23:41:42 -0700

    testing interactions

---
+++ backend/test/integration/InteractionsTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        // Set up so that user has a starting balance
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+
+        vm.deal(USER, STARTING_BALANCE);
+    }
+}
