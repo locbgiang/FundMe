173956b70d131baefd7eee9372d8bf04dfa68fb6
locgiang <locbgiang@gmail.com>
2024-12-12 10:12:09 -0800

    setting up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,15 @@
-import { useState } from 'react';
-import { ethers } from "ethers";
+import {Navbar} from "./components/Navbar";
+import { Body } from "./components/Body";
 
 function App() {
-  const [account, setAccount] = useState(null);
-
-  async function connectWallet() {
-    if (window.ethereum) {
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAccount(accounts[0]); // set the first account from metamask
-        console.log('Connect account: ', accounts[0]);
-      } catch (error) {
-        console.error('Error connecting wallet: ', error);
-      }
-    } else {
-      alert('Metamask is not installed, Please install Metamask. ')
-    }
-  }
-
   return (
     <div>
-      <button onClick={connectWallet}>
-        {account ? `Connected: ${account}` : 'Connect Wallet'}
-      </button>
+      <div>
+        <Navbar />
+      </div>
+      <Body />
     </div>
-  );
+  )
 }
 
 export default App
